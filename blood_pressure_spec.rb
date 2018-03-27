22dfe1fba119a61c5a678bcfc9eaaa1dab7cfffd
Israel Rivas <israelrivasgonzalez@gmail.com>
2018-03-27 10:48:38 +0200

    Adding argvs missing error test

---
+++ blood_pressure_spec.rb
@@ -0,0 +1,18 @@
+require './blood_pressure'
+
+describe BloodPressureLogger do
+  describe '#check_argvs' do
+    
+    let(:systolic) { 120 }
+    let(:diastolic) { 80 }
+    let(:pulse) { 60 }
+    
+    context 'when argvs are less than three' do
+      subject { described_class.new(systolic, diastolic) }
+
+      it 'shows argument missing error' do
+        expect{subject.check_argvs}.to raise_error('Argument missing!')
+      end
+    end
+  end
+end
