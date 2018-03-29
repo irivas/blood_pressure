b2142dfe70585e340acd4969e34b2e4d9c887e7e
Israel Rivas <israelrivasgonzalez@gmail.com>
2018-03-29 10:37:34 +0200

    Adding save_in_file specs

--- spec/blood_pressure_spec.rb
+++ spec/blood_pressure_spec.rb
@@ -2,8 +2,11 @@ require './lib/blood_pressure'
 
 describe BloodPressureLogger do
   let(:systolic) { 120 }
+  let(:systolic_2) { 125 }
   let(:diastolic) { 80 }
+  let(:diastolic_2) { 85 }
   let(:pulse) { 60 }
+  let(:pulse_2) { 65 }
 
   describe '#check_argvs' do
     context 'when argvs are less than three' do
@@ -25,13 +28,31 @@ describe BloodPressureLogger do
 
   describe '#print_log' do
     context 'when having the correct data' do
-      subject { described_class.new(systolic, diastolic, pulse) }
       let(:systolic_message) { "Systolic value added: #{systolic}" }
       let(:diastolic_message) { "Diastolic value added: #{diastolic}" }
       let(:pulse_message) { "Pulse value added: #{pulse}" }
+      
+      subject { described_class.new(systolic, diastolic, pulse) }
 
       it 'prints the correct message' do
         expect{subject.print_log}.to output("#{systolic_message}\n#{diastolic_message}\n#{pulse_message}\n").to_stdout
+      end
+    end
+  end
+  
+  describe '#save_in_file' do
+    context 'when having the correct data' do
+      let(:file) { 'pressure_measures.csv' }
+      
+      subject { described_class.new(systolic_2, diastolic_2, pulse_2) }
+
+      before { subject.save_in_file }
+      
+      it 'writes the proper data in file' do
+        lines = File.open(file).to_a
+        row = CSV.parse_line(lines.last.scrub(''), col_sep: ',')
+    
+        expect(row.drop(1)).to eq([systolic_2.to_s, diastolic_2.to_s, pulse_2.to_s])  
       end
     end
   end
