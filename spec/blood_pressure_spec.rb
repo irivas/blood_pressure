e5d4c848251238e1e1ae29afa253e22ea4509680
Israel Rivas <israelrivasgonzalez@gmail.com>
2018-03-29 11:45:37 +0200

    Fixing some Rubocop offenses

--- spec/blood_pressure_spec.rb
+++ spec/blood_pressure_spec.rb
@@ -11,7 +11,7 @@ describe BloodPressureLogger do
   describe '#check_argvs' do
     context 'when argvs are less than three' do
       subject { described_class.new(systolic, diastolic) }
-      
+
       it 'shows argument missing error' do
         expect{subject.check_argvs}.to raise_error(ArgumentError)
       end
@@ -19,7 +19,7 @@ describe BloodPressureLogger do
 
     context 'when argvs are three' do
       subject { described_class.new(systolic, diastolic, pulse) }
-      
+
       it 'does not show argument missing error' do
         expect{subject.check_argvs}.not_to raise_error
       end
@@ -31,7 +31,7 @@ describe BloodPressureLogger do
       let(:systolic_message) { "Systolic value added: #{systolic}" }
       let(:diastolic_message) { "Diastolic value added: #{diastolic}" }
       let(:pulse_message) { "Pulse value added: #{pulse}" }
-      
+
       subject { described_class.new(systolic, diastolic, pulse) }
 
       it 'prints the correct message' do
@@ -39,20 +39,20 @@ describe BloodPressureLogger do
       end
     end
   end
-  
+
   describe '#save_in_file' do
     context 'when having the correct data' do
       let(:file) { 'pressure_measures.csv' }
-      
+
       subject { described_class.new(systolic_2, diastolic_2, pulse_2) }
 
       before { subject.save_in_file }
-      
+
       it 'writes the proper data in file' do
         lines = File.open(file).to_a
         row = CSV.parse_line(lines.last.scrub(''), col_sep: ',')
-    
-        expect(row.drop(1)).to eq([systolic_2.to_s, diastolic_2.to_s, pulse_2.to_s])  
+
+        expect(row.drop(1)).to eq([systolic_2.to_s, diastolic_2.to_s, pulse_2.to_s])
       end
     end
   end
